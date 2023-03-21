b331cb0bcc6a2f330a9d279a93fa50973b71cafb
Samuell Marckson <samuellmarckson30@gmail.com>
2023-03-21 14:36:05 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/repository/OrderItemRepository.java
+++ src/main/java/com/educandoweb/course/repository/OrderItemRepository.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.repository;
 
 import com.educandoweb.course.entity.OrderItem;
-import com.educandoweb.course.entity.pk.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {

a980ddc6f50eb1c684b29b4cbf8a48e01e8f6031
Samuell Marckson <samuellmarckson30@gmail.com>
2023-03-21 14:57:14 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/educandoweb/course/entity/Payment.java
@@ -0,0 +1,68 @@
+package com.educandoweb.course.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Integer id, Instant momoment, Order order) {
+        this.id = id;
+        this.moment = momoment;
+        this.order = order;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getMomoment() {
+        return moment;
+    }
+
+    public void setMomoment(Instant momoment) {
+        this.moment = momoment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id) && Objects.equals(moment, payment.moment) && Objects.equals(order, payment.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, moment, order);
+    }
+}
